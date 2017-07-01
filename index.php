3c6d142e0d0cd7536756980a5534a3bc2eae361b
David <vargasd2106@gmail.com>
2017-06-30 22:35:28 -0600

    Conecté a base de datos

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Proyecto 1</title>
+</head>
+<body>
+	<header>
+		<h1>Probando</h1>
+	</header>
+	<?php
+	$conn = mysqli_connect("localhost", "root", "", "proyecto1");
+	$sql = "SELECT * FROM personas";
+	$result = mysqli_query($conn, $sql);
+	while($fila = mysqli_fetch_assoc($result)){
+		echo "<table><thead><tr><th>Id</th><th>Nombre</th></tr></thead>".
+		"<tbody><tr><td>".$fila['id']."</td><td>".$fila['nombre']."</td></tr></tbody></table>";
+	}
+	mysqli_close($conn);
+	?>
+</body>
+</html>
